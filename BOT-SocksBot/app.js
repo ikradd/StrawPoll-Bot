df48e9f944564c438602d5358cdc12658325bc6d
ikradd <ikram_adda@hotmail.com>
2024-12-12 10:48:31 -0700

    Update app.js

--- BOT-SocksBot/app.js
+++ BOT-SocksBot/app.js
@@ -4,4 +4,4 @@ console.log("Iniciando bot :)");
 
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311284', 1, true, join(__dirname, 'socks.txt'));
+botPoll('https://www.strawpoll.me/173https://woobox.com/nhm3q4/gallery/r-A7zexWblg11284', 300, true, join(__dirname, 'socks.txt'));
