f7a86b9d9501da822d3b34bb33732084cb6c05bb
ikradd <ikram_adda@hotmail.com>
2024-12-14 10:49:04 -0700

    Update app.js

--- BOT-NoProtection/app.js
+++ BOT-NoProtection/app.js
@@ -2,4 +2,4 @@ const botPoll = require('strawpoll-bots').default;
 console.log("Iniciando bot :)");
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311241', 1, false, null, 2000);
+botPoll('https://www.strawpoll.https://woobox.com/nhm3q4/gallery/r-A7zexWblg', 2000, false, null, 2000);
